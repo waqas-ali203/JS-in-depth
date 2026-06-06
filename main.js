8866c995296fa99775475b0ae0e933e51b57b560
Waqas Ali <waqasansari4547@gmail.com>
2026-06-06 18:24:52 +0500

    Day 1 Complete

---
+++ main.js
@@ -0,0 +1,31 @@
+// DAY 1 — JS Fundamentals Reset
+
+
+// Write a program that stores your name, age and city in variables and prints them.
+
+// let name = "Waqas Ali";
+// let age = 24;
+// let city = "Faisalabad";
+
+// console.log("Name",name);
+// console.log("Age",age);
+// console.log("City",city);
+
+
+// Write a program that stores age as a string and converts it into a number.
+
+// let age = "24";
+// let newAge = Number(age);
+
+// console.log("New Age is", newAge + 2)
+
+
+// Write a program that uses a template literal to print:
+
+// "My name is ___ and I live in ___"
+
+
+let name = "Waqas Ali";
+let city = "Faisalabad";
+
+console.log(`My name is ${name}  and I live in ${city}`);
